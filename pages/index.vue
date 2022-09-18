751b95cee21c2e49e2e1da7d243a50ae995f5937
ChurikiTenna <52132649+ChurikiTenna@users.noreply.github.com>
2022-09-18 14:13:42 +0900

    p

--- pages/index.vue
+++ pages/index.vue
@@ -18,8 +18,8 @@ const leadingsText = computed(() => [
   },
   {
     text: titlesText.value[1],
-    startColor: '#7928CA',
-    endColor: '#FF0080',
+    startColor: '#007CF0',
+    endColor: '#00DFD8',
     delay: 2,
   },
   {
@@ -48,7 +48,7 @@ const copyBash = () => {
 
 <template>
   <PageWrapper class="flex-1 flex">
-    <div class="background-overlay">
+    <!--<div class="background-overlay">
       <div
         class="absolute top-0 left-0 transform translate-x-64 translate-y-4 h-14 w-14 rounded-full bg-gray-900 dark:bg-white"
       ></div>
@@ -59,7 +59,7 @@ const copyBash = () => {
         class="absolute hidden md:block bottom-0 right-0 transform -translate-x-4 -translate-y-40 h-16 w-16 rounded bg-purple-600 linear-wipe"
       ></div>
       <div class="absolute bottom-0 right-0 triangle-shape"></div>
-    </div>
+    </div>-->
     <PageBody class="flex-1 flex">
       <PageSection class="flex-1 flex items-center">
         <div class="flex-1 md:w-2/3 flex flex-col z-10">
@@ -80,7 +80,7 @@ const copyBash = () => {
           <div
             class="flex space-x-4 ml-4 mt-10 justify-center md:justify-start"
           >
-            <Button
+            <!--<Button
               size="lg"
               text="Nuxt 3"
               class="font-extrabold"
@@ -92,19 +92,12 @@ const copyBash = () => {
               type="secondary"
               class="font-extrabold"
               href="https://github.com/viandwi24/nuxt3-awesome-starter"
-            />
+            />-->
           </div>
         </div>
         <div class="hidden md:flex flex-1 justify-center items-end relative">
           <Gem class="absolute -top-64 -right-0" />
-          <div class="ml-4 w-100 z-10 h-auto shadow">
-            <div
-              class="win-header bg-gray-200 dark:bg-slate-800 flex flex space-x-4 px-3 py-2 rounded-t-lg relative border-b-2 border-gray-300/75 dark:border-slate-700/75"
-            >
-              Text
-              
-            </div>
-          </div>
+            
         </div>
       </PageSection>
     </PageBody>
