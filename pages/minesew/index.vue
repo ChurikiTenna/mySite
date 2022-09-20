5a022c824e38a9acaedac2a2a883284452478c64
ChurikiTenna <52132649+ChurikiTenna@users.noreply.github.com>
2022-09-20 09:01:27 +0900

    p

---
+++ pages/minesew/index.vue
@@ -0,0 +1,79 @@
+<script lang="ts" setup>
+  import { capitalize } from '~/utils/str'
+  
+  // composable
+  const { t } = useLang()
+  const pageName = "minesew"
+  const privacyPolicyUrl = pageName+"/privacyPolicy"
+  const termOfUseUrl = pageName+"/termOfUse"
+  // compiler macro
+  definePageMeta({
+    layout: 'page',
+  })
+  useHead(() => ({
+    title: capitalize(pageName),
+    meta: [
+      {
+        name: 'description',
+        content: pageName+'についてのページです',
+      },
+    ],
+  }))
+  </script>
+  
+  <template>
+    <PageWrapper>
+      <PageHeader>
+        <PageTitle :text="pageName" class="capitalize" />
+      </PageHeader>
+      <PageBody>
+        <PageSection>
+          
+            <div class="block hover:no-underline p-6 flex space-x-6">
+              <div class="mt-1 text-slate-600 dark:text-gray-400 text-right">
+                <img
+                  style="width: 100px; border-radius: 24%"
+                  src="https://firebasestorage.googleapis.com/v0/b/churiki-tenna.appspot.com/o/minesew.png?alt=media&token=881e9b0f-281e-4bb1-b1d4-15f2f20f5d1d"
+                  alt="icon">
+              </div>
+              <div class="flex flex-col">
+                
+                <div class="text-slate-700 dark:text-gray-100 mb-2">
+                  自分の作ったものをシェアするコミュニティです。
+                  <br>投稿、いいね、コメント機能のみのシンプルなアプリです。
+                </div>
+
+                <div class="text-slate-700 dark:text-gray-300 mb-2" style="font-size: 14px">
+                  iOSアプリ
+                </div>
+                <div class="flex">
+                  <Anchor
+                    class="text-sm flex space-x-1 items-center text-primary-500"
+                    to="https://apps.apple.com/us/app/minesew/id1645837979"
+                  >
+                    <span>{{ $t('others.learn_more') }}</span>
+                    <icon:ic:baseline-arrow-right-alt class="text-sm" />
+                  </Anchor>
+                </div>
+              </div>
+            </div>
+            <center>
+              <Anchor
+                  :to="privacyPolicyUrl"
+                  class="flex-1 hover:no-underline"
+                  style="padding: 10px"
+                  >・プライバシーポリシー</Anchor
+                >
+            <Anchor
+                  :to="termOfUseUrl"
+                  class="flex-1 hover:no-underline"
+                  style="padding: 10px"
+                  >・利用規約</Anchor
+                >
+            </center>
+            
+        </PageSection>
+      </PageBody>
+    </PageWrapper>
+  </template>
+  
---
+++ pages/minesew/termOfUse.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+  import { capitalize } from '~/utils/str'
+  
+  // composable
+  const { t } = useLang()
+  const appName = "minesew"
+  // compiler macro
+  definePageMeta({
+    layout: 'page',
+  })
+  useHead(() => ({
+    title: capitalize(appName+" | 利用規約"),
+    meta: [
+      {
+        name: 'description',
+        content: appName+'の利用規約ページです',
+      },
+    ],
+  }))
+  </script>
+  
+  <template>
+    <PageWrapper>
+      <PageHeader>
+        <PageTitle :text="appName" class="capitalize" />
+      </PageHeader>
+      <PageBody>
+        <PageSection>
+          Text
+        </PageSection>
+      </PageBody>
+    </PageWrapper>
+  </template>
